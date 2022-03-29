033f7ce7a32e8c5fcf5ac1c3c35083cc76c01cf1
Baptiste852 <77391285+Baptiste852@users.noreply.github.com>
2022-03-29 23:56:37 +0200

    Update BEnreg.java

--- src/BEnreg.java
+++ src/BEnreg.java
@@ -1,43 +1,13 @@
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-
 import javax.swing.JButton;
 
-public class BEnreg extends JButton implements MouseListener {
+public class BEnreg extends JButton {
 	
 	public BEnreg() {
 		this.setLabel("Enregistrer le modèle");
 		this.setPreferredSize(getMaximumSize());
-		this.addMouseListener(this);
 	}
 
 	public static void main(String[] args) {
 		
 	}
-
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		
-	}
-
-	@Override
-	public void mousePressed(MouseEvent e) {
-		
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		
-	}
-
 }
