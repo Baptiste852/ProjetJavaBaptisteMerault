08220170d5557918f0dda9c6bb9a1b78f72c8e9d
Baptiste852 <77391285+Baptiste852@users.noreply.github.com>
2022-03-29 14:00:13 +0200

    Update BibliModeles.java

--- src/BibliModeles.java
+++ src/BibliModeles.java
@@ -1,12 +1,11 @@
 import java.awt.List;
-
 import javax.swing.JPanel;
 
 public class BibliModeles extends JPanel {
 	
 	public BibliModeles() {
 		String[] tabModeles= {"modele 1","modele 2","modele 3","modele 4","modele 5"};
-		List jl=new List(20);
+		List jl=new List(10);
 		for (int i=0; i<tabModeles.length; i++) {
 			jl.add(tabModeles[i]);
 		}
