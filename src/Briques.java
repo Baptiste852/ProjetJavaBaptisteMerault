f830df278b00026e8f292d48f0203d8993e2f8fa
Baptiste852 <77391285+Baptiste852@users.noreply.github.com>
2022-03-29 14:02:46 +0200

    Create Briques.java

---
+++ src/Briques.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Briques extends ArrayList<String> {
+	
+	public Briques() {
+		this.add("C:/Users/Baptiste/Desktop/brique_rouge.jpg");
+		this.add("C:/Users/Baptiste/Desktop/brique_bleu.jpg");
+		this.add("C:/Users/Baptiste/Desktop/brique_verte.jpg");
+	}
+
+	public static void main(String[] args) {
+		
+	}
+
+}
