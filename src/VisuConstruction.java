6e7dcafec8fe2ecd2178f8e3caf6ac522349311f
Baptiste852 <77391285+Baptiste852@users.noreply.github.com>
2022-03-29 14:01:06 +0200

    Update VisuConstruction.java

--- src/VisuConstruction.java
+++ src/VisuConstruction.java
@@ -1,10 +1,87 @@
 import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
-public class VisuConstruction extends Canvas {
-
+public class VisuConstruction extends Canvas implements MouseListener,KeyListener {
+	
+	Briques br=new Briques();
+	String imageDefault =br.get(0);
+	Image img = null;
+	
+	public VisuConstruction() {
+		this.setBackground(Color.cyan);
+		this.addMouseListener(this);
+		this.setPreferredSize(new Dimension(640,360));
+		this.addKeyListener(this);
+		try {
+			img = ImageIO.read(new File(imageDefault));
+		} catch (IOException e1) {
+			throw new RuntimeException("L'image "+imageDefault+" est introuvable");
+		}
+	}
+	
+	public void paint2(Graphics g,int x,int y) {
+		g.drawImage(img, x, y, 50, 50, null);
+	}
+	
 	public static void main(String[] args) {
 		
 		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		paint2(getGraphics(),e.getX(),e.getY());
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyChar()=='a') {
+			
+		} else if (e.getKeyChar()=='z') {
+			
+		} else if (e.getKeyChar()=='e') {
+			
+		}
 	}
 
 }
