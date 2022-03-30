594f7f7dcc2286cb7f48a8a39d7e96cc81775a19
Baptiste852 <77391285+Baptiste852@users.noreply.github.com>
2022-03-30 11:45:52 +0200

    Create BEnreg.java

---
+++ src/BEnreg.java
@@ -0,0 +1,13 @@
+import javax.swing.JButton;
+
+public class BEnreg extends JButton {
+	
+	public BEnreg() {
+		this.setLabel("Enregistrer le modèle");
+		this.setPreferredSize(getMaximumSize());
+	}
+
+	public static void main(String[] args) {
+		
+	}
+}
