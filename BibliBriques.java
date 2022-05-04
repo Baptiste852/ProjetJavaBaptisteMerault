4861d5c1ab3fb2b8333ad40cb584e75264362a0c
Baptiste852 <77391285+Baptiste852@users.noreply.github.com>
2022-05-04 16:08:53 +0200

    Create BibliBriques.java

---
+++ BibliBriques.java
@@ -0,0 +1,44 @@
+package application;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.List;
+import java.util.HashMap;
+import java.util.Map;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class BibliBriques extends JPanel {
+	
+	public static Map<String, Color> couleurs = new HashMap<>();
+	JButton brouge = new JButton("brique 1");
+	JButton bbleu=new JButton("brique 2");
+	JButton bvert=new JButton("brique 3");
+	
+	public BibliBriques(Controleur c) {
+		String[] tabBriques= {"taille 50/30","  rouge","    brique 1","  bleu","    brique 2","  vert","    brique 3"};
+		List jl=new List(10);
+		for (int i=0; i<tabBriques.length; i++) {
+			jl.add(tabBriques[i]);
+		}
+		this.add(jl);
+		JButton brouge = new JButton("brique 1");
+		JButton bbleu=new JButton("brique 2");
+		JButton bvert=new JButton("brique 3");
+		brouge.setPreferredSize(new Dimension(150,50));
+		brouge.setActionCommand("rouge");
+		couleurs.put("rouge", Color.red);
+		brouge.addActionListener(c);
+		setPreferredSize(new Dimension(150,50));
+		bbleu.setActionCommand("bleu");
+		couleurs.put("bleu", Color.blue);
+		bbleu.addActionListener(c);
+		setPreferredSize(new Dimension(150,50));
+		bvert.setActionCommand("vert");
+		couleurs.put("vert", Color.green);
+		bvert.addActionListener(c);
+		this.add(brouge);
+		this.add(bbleu);
+		this.add(bvert);
+	}
+	
+}
