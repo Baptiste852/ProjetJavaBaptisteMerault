6b679f9c4dcc97ca2a4d145af885a4d43905a8e6
Baptiste852 <77391285+Baptiste852@users.noreply.github.com>
2022-05-04 16:11:23 +0200

    Create Brique.java

---
+++ Brique.java
@@ -0,0 +1,16 @@
+package application;
+import java.awt.Color;
+
+public class Brique {
+	
+	int x,y;
+	Color couleur;
+	
+	public Brique(int x, int y, Color c) {
+		this.x = x;
+		this.y = y;
+		this.couleur = c;
+	}
+	
+
+}
