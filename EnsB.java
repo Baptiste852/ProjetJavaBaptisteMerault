718cb68cadeeb2fa43e00ceb774602f0f1272d44
Baptiste852 <77391285+Baptiste852@users.noreply.github.com>
2022-05-13 10:32:54 +0200

    Update EnsB.java

--- EnsB.java
+++ EnsB.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.awt.Color;
+
 import javax.swing.JPanel;
 
 public class EnsB extends JPanel {
@@ -7,6 +9,7 @@ public class EnsB extends JPanel {
 	BibliModeles bm=new BibliModeles();
 	
 	public EnsB() {
+		this.setBackground(Color.ORANGE);
 		this.add(bm.bm1);
 		this.add(bm.bm2);
 	}
